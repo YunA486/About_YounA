889feb2983225d272bfd440f54495a7da9bcaf15
joyun <w2039@e-mirim.hs.kr>
2021-07-27 23:47:09 +0900

    Edit activity_main.xml(main) Commit

--- app/src/main/java/kr/hs/emirim/about_youna/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/about_youna/MainActivity.java
@@ -12,21 +12,25 @@ public class MainActivity extends TabActivity {
         setContentView(R.layout.activity_main);
         TabHost tabHost = getTabHost();
 
-        TabHost.TabSpec t1 = tabHost.newTabSpec("one").setIndicator("Photo 01");
-        t1.setContent(R.id.img01);
+        TabHost.TabSpec t1 = tabHost.newTabSpec("one").setIndicator("", getResources().getDrawable(R.drawable.web));
+        t1.setContent(R.id.layout01);
         tabHost.addTab(t1);
 
-        TabHost.TabSpec t2 = tabHost.newTabSpec("two").setIndicator("Photo 02");
-        t2.setContent(R.id.img02);
+        TabHost.TabSpec t2 = tabHost.newTabSpec("two").setIndicator("", getResources().getDrawable(R.drawable.android));
+        t2.setContent(R.id.layout02);
         tabHost.addTab(t2);
 
-        TabHost.TabSpec t3 = tabHost.newTabSpec("three").setIndicator("Photo 03");
-        t3.setContent(R.id.img03);
+        TabHost.TabSpec t3 = tabHost.newTabSpec("three").setIndicator("", getResources().getDrawable(R.drawable.home));
+        t3.setContent(R.id.layout03);
         tabHost.addTab(t3);
 
-        TabHost.TabSpec t4 = tabHost.newTabSpec("four").setIndicator("Photo 04");
-        t4.setContent(R.id.img04);
+        TabHost.TabSpec t4 = tabHost.newTabSpec("four").setIndicator("", getResources().getDrawable(R.drawable.game));
+        t4.setContent(R.id.layout04);
         tabHost.addTab(t4);
+
+        TabHost.TabSpec t5 = tabHost.newTabSpec("five").setIndicator("", getResources().getDrawable(R.drawable.consol));
+        t5.setContent(R.id.layout05);
+        tabHost.addTab(t5);
 
         tabHost.setCurrentTab(1);
     }
